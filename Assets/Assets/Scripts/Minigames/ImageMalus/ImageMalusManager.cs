55a905e3675075ad950b4b5e95dab6efcf65542a
jeremy.lombard <jeremylombard.mail@gmail.com>
2025-09-03 23:21:23 +0200

    Image Malus Base Scene Done

--- Assets/Assets/Scripts/Minigames/ImageMalus/ImageMalusManager.cs
+++ Assets/Assets/Scripts/Minigames/ImageMalus/ImageMalusManager.cs
@@ -1,5 +1,6 @@
+using System;
 using UnityEngine;
-using UnityEngine.Rendering;
+using Random = UnityEngine.Random;
 
 public class ImageMalusManager : MonoBehaviour
 {
@@ -8,6 +9,26 @@ public class ImageMalusManager : MonoBehaviour
     [SerializeField] private Transform[] arrayOfGlasses;
 
     [SerializeField] private Transform[] arrayOfSelectedSpot;
+
+    private Transform currentGlassTransform;
+    
+    private Vector2 mousePos;
+    public bool IsDraged = false;
+    [SerializeField] private LayerMask movableLayers;
+    
+    public static ImageMalusManager ImageMalusManagerInstance;
+
+    private bool asWin;
+    
+    
+    void Awake()
+    {
+        if (ImageMalusManagerInstance == null)
+        {
+            ImageMalusManagerInstance = this;
+        }
+    }
+    
     void Start()
     {
         arrayOfGlasses = parentOfGlasses.GetComponentsInChildren<Transform>();
@@ -17,7 +38,47 @@ public class ImageMalusManager : MonoBehaviour
             arrayOfSelectedSpot[i].transform.position = arrayOfGlasses[i].transform.position;
         }
     }
-    
+
+    private void Update()
+    {
+        if (asWin == false)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Input.GetMouseButton(0))
+            {
+
+                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, float.PositiveInfinity, movableLayers);
+
+                if (hit)
+                {
+                    IsDraged = true;
+                    currentGlassTransform = hit.transform;
+                }
+            }
+            else
+            {
+                IsDraged = false;
+            }
+
+            if(IsDraged)
+            {
+                currentGlassTransform.transform.position = mousePos;
+            }
+            //Winning condition
+            foreach (var spot in arrayOfSelectedSpot)
+            {
+                if (spot.GetComponent<DragableGlassSlot>().IsComplete == false)
+                {
+                    return;
+                }
+            }
+            asWin = true;
+            GameManager.Instance.RandomizeNextState(); //next State
+        }
+    }
+
+
     public void GenerateGlassesParentPosition()
     {
         for (int i = 0; i < arrayOfGlasses.Length; i++)
